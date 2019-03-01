f9999dfd659ce9d7f37012298c9bbe81fd7e8642
Chris Armata <chris.armata@gmail.com>
2019-03-01 00:09:34 -0500

    Track wins/losses and add game reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,9 @@
 var gameData = {
   random: 0,
   crystals: [0, 0, 0, 0],
-  total: 0
+  total: 0,
+  wins: 0,
+  losses: 0
 };
 
 function randNum() {
@@ -18,6 +20,10 @@ function randCrystal() {
 function newGame() {
   randNum();
   randCrystal();
+  gameData.total = 0;
+  $("#score").text(gameData.total);
+  $("#wins").text(gameData.wins);
+  $("#losses").text(gameData.losses);
 }
 
 $(document).ready(function() {
@@ -25,5 +31,13 @@ $(document).ready(function() {
   $("#crystals img").on("click", function() {
     gameData.total += gameData.crystals[$(this).attr("data-attr") - 1];
     $("#score").text(gameData.total);
+
+    if (gameData.total === gameData.random) {
+      gameData.wins++;
+      newGame();
+    } else if (gameData.total > gameData.random) {
+      gameData.losses++;
+      newGame();
+    }
   });
 });
